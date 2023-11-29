ce488f8aac0ecbaf1a5a95b4bef5cfadb224ffbc
josh-melton-db <josh-melton-db>
2023-11-29 13:02:49 +0000

    Changed table naming

--- utils/onboarding_setup/onboarding_setup.py
+++ utils/onboarding_setup/onboarding_setup.py
@@ -3,15 +3,15 @@ import random
 def get_config(spark):
   num_rows = random.randint(1000, 2000)
   current_user = spark.sql('select current_user()').collect()[0][0].split('@')[0].replace('.', '_')
-  schema = f'{current_user}_onboarding'
+  schema = f'onboarding'
   return {
       'current_user': current_user,
       'schema' : schema,
-      'bronze_table' : f'{schema}.sensor_bronze',
-      'defect_table' : f'{schema}.defect_bronze',
-      'silver_table' : f'{schema}.sensor_silver',
-      'gold_table' : f'{schema}.sensor_gold',
-      'tuned_bronze_table' : f'{schema}.sensor_bronze_clustered',
+      'bronze_table' : f'{schema}.{current_user}_sensor_bronze',
+      'defect_table' : f'{schema}.{current_user}_defect_bronze',
+      'silver_table' : f'{schema}.{current_user}_sensor_silver',
+      'gold_table' : f'{schema}.{current_user}_sensor_gold',
+      'tuned_bronze_table' : f'{schema}.{current_user}_sensor_bronze_clustered',
       'csv_staging' : f'/{current_user}_onboarding/csv_staging',
       'checkpoint_location' : f'/{current_user}_onboarding/sensor_checkpoints',
       'rows_per_run' : num_rows,
